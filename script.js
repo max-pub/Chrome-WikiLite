b9d4091d3fc3183f76ebbd47d3cd4ac7fdc3fd60
Max Fechner <max.fechner@gmail.com>
2016-02-06 15:17:29 +0700

    copied local code

---
+++ script.js
@@ -0,0 +1,20 @@
+// var tmp = document.getElementById('mw-panel');
+// tmp.parentNode.removeChild(tmp);
+
+// var tmp = document.getElementById('mw-navigation');
+// tmp.parentNode.removeChild(tmp);
+
+// var tmp = document.getElementById('mw-page-base');
+// tmp.parentNode.removeChild(tmp);
+
+// var tmp = document.getElementById('mw-head-base');
+// tmp.parentNode.removeChild(tmp);
+
+// var tmp = document.getElementsByClassName('Vorlage_Belege_fehlen')[0];
+// tmp.parentNode.removeChild(tmp);
+
+// var tmp = document.getElementById('footer');
+// tmp.parentNode.removeChild(tmp);
+
+var tmp = document.getElementById('content');
+tmp.removeAttribute('class');
